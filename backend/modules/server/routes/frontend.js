656b10bef007fce356f34b687ab6f25466a54a22
goliatone <burgosemi@gmail.com>
2020-04-05 23:49:12 -0700

    update deps

--- backend/modules/server/routes/frontend.js
+++ backend/modules/server/routes/frontend.js
@@ -1,12 +1,23 @@
 'use strict';
+const marked = require('marked');
+const { readFileSync } = require('fs');
 
 module.exports.init = function(server, config) {
     server.logger.info('Create frontend route...');
 
     server.get('/', (req, res) => {
         res.renderFile('index', {
-            baseUrl: config.baseUrl,
-            token: config.token
+            baseUrl: config.frontend.baseUrl,
+            token: config.frontend.token
+        });
+    });
+
+    server.get('/about', (req, res) => {
+        const content = readFileSync(config.frontend.bodyPath).toString();
+        res.renderFile('about', {
+            title: config.frontend.title,
+            // body: marked(config.frontend.body.toString())
+            body: marked(content)
         });
     });
 };
