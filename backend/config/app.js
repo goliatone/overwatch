edf53ebc063570a1121a787d52a9d24b6a29a274
goliatone <burgosemi@gmail.com>
2020-04-06 00:15:18 -0700

    update banner

--- backend/config/app.js
+++ backend/config/app.js
@@ -1,14 +1,14 @@
 'use strict';
 
 /*
- * http://patorjk.com/software/taag/#p=display&h=2&v=2&c=bash&w=.&f=ANSI%20Shadow&t=svelte-front-end-template
+ * http://patorjk.com/software/taag/#p=display&h=2&v=2&c=bash&w=.&f=ANSI%20Shadow&t=Overwatch
  */
 const banner = require('fs').readFileSync('./config/app.banner.txt', 'utf-8');
 const resolve = require('path').resolve;
 
 module.exports = {
     banner,
-    name: 'Svelte Front End Template',
+    name: 'Overwatch Data Server',
     basepath: resolve('./'),
     environment: process.env.NODE_ENV || 'development',
     registry: false
