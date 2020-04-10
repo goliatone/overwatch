02786da41e072ab113e00a8d2ea99ebdae3cfdba
goliatone <burgosemi@gmail.com>
2020-04-10 12:10:30 -0700

    add search bar component

---
+++ frontend/src/components/SearchBar.svelte
@@ -0,0 +1,61 @@
+<script>
+    import {filters} from '../stores/filters';
+    import incidents from '../stores/incidents';
+    import {categories} from '../consts';
+
+    //on:keyup={handleKeyup}
+    function handleKeyup(e) {
+        if(event.keyCode !== 13) return;
+        const value = e.target.value.trim().toLowerCase();
+        
+        if(value === '') $filters = [];
+
+        if(categories.indexOf(value) > -1 && $filters.indexOf(value) === -1) {
+            $filters = [...$filters, value];
+        }
+    }
+
+    const unsubscribeFilters = filters.subscribe(newFilters => {
+        console.log('filters updated', newFilters);
+        const codeLabel = newFilters[0];
+
+        const onLoadQuery = { page: 1, size: 200 };
+        
+        if(codeLabel) onLoadQuery.codeLabel = codeLabel;
+
+        // const onLoadQuery = { page: 1, size: 200, where: {codeLabels: $filters} };
+        incidents.listItems('sacramento', onLoadQuery);
+    });
+
+</script>
+
+<style>
+.filter-ctrl {
+    position: absolute;
+    top: 10px;
+    right: 50px;
+    z-index: 1;
+}
+ 
+.filter-ctrl input[type='text'] {
+    font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
+    width: 100%;
+    border: 0;
+    background-color: #fff;
+    margin: 0;
+    color: rgba(0, 0, 0, 0.5);
+    padding: 10px;
+    box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.1);
+    border-radius: 3px;
+    width: 180px;
+}
+</style>
+
+<div class="filter-ctrl">
+    <input  id="filter-input"
+            on:keypress={handleKeyup}
+            type="text"
+            name="filter"
+            placeholder="Filter by name"
+    />
+</div>
