64696d775894375f0279c5068f0a6f981d7c7d79
goliatone <burgosemi@gmail.com>
2020-04-10 23:16:40 -0700

    add Tag to SearchBar

--- frontend/src/components/SearchBar.svelte
+++ frontend/src/components/SearchBar.svelte
@@ -3,6 +3,17 @@
     import incidents from '../stores/incidents';
     import {categories} from '../consts';
 
+    import Tags from './Tags.svelte'
+
+    /**
+     * We want to use TAB, ENTER, and COMMA
+     */ 
+    let addKeys = [9,13,188];
+
+    function handleTagProperties(e) {
+        let tags = e.detail.tags;
+        $filters = tags;
+    }
     //on:keyup={handleKeyup}
     function handleKeyup(e) {
         if(event.keyCode !== 13) return;
@@ -16,13 +27,11 @@
     }
 
     const unsubscribeFilters = filters.subscribe(newFilters => {
-        console.log('filters updated', newFilters);
-        const codeLabel = newFilters[0];
-
         const onLoadQuery = { page: 1, size: 200 };
         
-        if(codeLabel) onLoadQuery.codeLabel = codeLabel;
-
+        if(newFilters && newFilters.length > 0) {
+            onLoadQuery.where = {codeLabel: newFilters};
+        }
         // const onLoadQuery = { page: 1, size: 200, where: {codeLabels: $filters} };
         incidents.listItems('sacramento', onLoadQuery);
     });
@@ -33,10 +42,11 @@
 .filter-ctrl {
     position: absolute;
     top: 10px;
-    right: 50px;
+    margin-left: 10px;
+    /* right: 50px; */
     z-index: 1;
 }
- 
+/* 
 .filter-ctrl input[type='text'] {
     font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
     width: 100%;
@@ -48,14 +58,23 @@
     box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.1);
     border-radius: 3px;
     width: 180px;
-}
+}*/
 </style>
 
 <div class="filter-ctrl">
-    <input  id="filter-input"
+    <!-- <input  id="filter-input"
             on:keypress={handleKeyup}
             type="text"
             name="filter"
             placeholder="Filter by name"
-    />
+    /> -->
+
+
+
+    <Tags   {addKeys} 
+            onlyUnique={true} 
+            autoComplete={categories} 
+            on:tags={handleTagProperties}
+            placeholder="Filter by category"
+            />
 </div>
