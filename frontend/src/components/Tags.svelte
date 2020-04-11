feb0a8d85a7463aa095c4b67720282b0409aa64f
goliatone <burgosemi@gmail.com>
2020-04-10 23:17:47 -0700

    add keyboard handlers to Tags

--- frontend/src/components/Tags.svelte
+++ frontend/src/components/Tags.svelte
@@ -1,9 +1,14 @@
 <script>
     import {createEventDispatcher} from 'svelte';
-
+        
     const dispatch = createEventDispatcher();
     let tag;
     let arrelementsmatch = [];
+    let liIndex = -1;
+
+    let container;
+
+    let removeComma = false;
 
     export let tags;
     export let addKeys;
@@ -15,6 +20,18 @@
     export let allowDrop;
     export let splitWith;
     export let autoComplete;
+
+    /**
+     * More here https://keycode.info/
+     */ 
+    const CODES = {
+        BACKSPACE: 8,
+        TAB: 9,
+        ENTER:13,
+        ARROW_UP: 38,
+        ARROW_DOWN: 40,
+        COMMA: 188,
+    };
 
     $: tags = tags || [];
     $: addKeys = addKeys || false;
@@ -28,82 +45,122 @@
     $: autoComplete = autoComplete || false;
 
     function setTag(event) {
-        
-        const currentTag = event.target.value;
+        // handleKeyPress(event);
+        const keyCode = event.keyCode;
+        let currentTag = event.target.value;
 
-        if (event.keyCode === 13) {
-            addTag(currentTag);
+        //On ENTER press we add the tag
+        if(keyCode === CODES.ENTER) {
+            if(arrelementsmatch.length > 0) {
+                let index = liIndex === -1 ? 0 : liIndex;
+                currentTag = arrelementsmatch[index];
+            }
+            
+            return addTag(currentTag);
         }
         
-        if (event.keyCode === 8 && tag == '') {
+        //If we BACKSPACE when there's no text then remove previous tag
+        if(keyCode === CODES.BACKSPACE && tag === '') {
             tags.pop();
             tags = tags;
-
+            liIndex = -1;
             dispatch('tags', {tags});
         }
         
-        if (addKeys) {
+        /**
+         * We can add tags by:
+         * - type tag + hitting ENTER
+         * - type tag + hitting TAB
+         * - type tag + typing "," between tags
+         * - type tag + typing random key added to addKeys[]
+         */
+        if(Array.isArray(addKeys)) {
             addKeys.forEach(key => {
-                if (key === event.keyCode) {
-                    switch (event.keyCode) {
-                    case 188:
-                        // Remove comma if keycode to add tag is comma
-                        addTag(currentTag.substring(0, currentTag.length - 1));
+                if(key !== keyCode) return;
+                switch (keyCode) {
+                    case CODES.COMMA:
+                        /**
+                         * We want to remove the trailing comma before
+                         * we add the word as a tag
+                         */ 
+                        removeComma = true;
+                        addTag(_trimTrailing(currentTag, ','));
                         break;
-                    case 9:
+                    case CODES.TAB:
                         event.preventDefault();
-                        addTag(currentTag);
+                        let index = liIndex === -1 ? 0 : liIndex;
+                        let match = arrelementsmatch[index];
+                        addTagFromDropdown(currentTag, match);
                         break;
                     default:
                         addTag(currentTag);
                         break;
-                    }
                 }
             });
         }
         
-        if (removeKeys) {
+        if(Array.isArray(removeKeys)) {
             removeKeys.forEach(key => {
-                if (key === event.keyCode) {
-                    tags.pop();
-                    tags = tags;
-                    tag = '';
-
-                    dispatch('tags', {tags});
-                }
+                if(key !== keyCode) return;
+                tags.pop();
+                tags = tags;
+                tag = '' ;
+                liIndex = -1;
+                dispatch('tags', {tags});
             });
         }
     }
 
-    function addTag(currentTag) {
-        currentTag = currentTag.trim();
-
-        if (currentTag == '') return;
-        if (maxTags && tags.length == maxTags) return;
+    function addTagFromDropdown(currentTag, match) {
         
+        console.log('add tag from dropdown', currentTag, match);
+        if(match) {
+            if(currentTag === match) {
+                tag = '';
+            } else if(match.indexOf(currentTag) !== -1) {
+                currentTag = tag = match;
+            }
+        }
+
         if(onlyUnique) {
-            if (tags.includes(currentTag)) return;
+            arrelementsmatch = arrelementsmatch.filter(m => m !== currentTag );
         }
         
+        addTag(currentTag);
+    }
+
+    function addTag(currentTag='') {
+        currentTag = currentTag.trim();
+        if(!currentTag) return;
+        
+        //TODO: We might want to move this to a function where we check
+        if(maxTags && tags.length === maxTags) return;
+
+        if(onlyUnique && tags.includes(currentTag)) return;
+
         tags.push(currentTag);
         tags = tags;
         tag = '';
-
+        liIndex = -1;
         dispatch('tags', {tags});
     }
 
     function removeTag(i) {
         tags.splice(i, 1);
         tags = tags;
-
+        liIndex = -1;
         dispatch('tags', {tags});
     }
 
+    /**
+     * Handle pasting a string on tags in our field
+     * https://developer.mozilla.org/en-US/docs/Web/API/Element/paste_event
+     */ 
     function onPaste(e) {
         if(!allowPaste) return;
         e.preventDefault();
         const data = getClipboardData(e);
-        const tags = splitTags(data).map(tag => addTag(tag));
+        const tags = splitTags(data).forEach(addTag);
     }
 
     function onDrop(e) {
@@ -114,62 +171,120 @@
     }
 
     function getClipboardData(e) {
-        if (window.clipboardData) {
-            return window.clipboardData.getData('Text');
+        if(window && window.clipboardData) {
+            return window.clipboardData.getData('text');
         }
-
-        if (e.clipboardData) {
+        if(e.clipboardData) {
             return e.clipboardData.getData('text/plain');
         }
-
         return '';
     }
 
     function splitTags(data) {
-        return data.split(splitWith).map(d => d.trim());
+        if(!data) return [];
+        return data.split(splitWith).map(t => t.trim());
     }
 
     function getMatchElements(e) {
+        if(!Array.isArray(autoComplete)) return;
 
-        if(!autoComplete) return;
-        
-        var x = e.target.value;
-        
-        if (x == '') {
-            arrelementsmatch = [];
-            return;
+        let term = e.target.value;
+        if(!term) return arrelementsmatch = [];
+
+        /**
+         * Select elements that match the given
+         * string...
+         */ 
+        let matched = autoComplete.filter(item => {
+            return item.toLowerCase().includes(term.toLowerCase());
+        });
+
+        if(onlyUnique) {
+            matched = matched.filter(m => tags.indexOf(m) === -1 );
         }
 
-        var matchs = autoComplete.filter(e =>e.toLowerCase().includes(x.toLowerCase()));
+        arrelementsmatch = matched;
+    }
 
-        arrelementsmatch = matchs;
+    function handleWindowClick(event) {
+        if(!container) return;
+        const eventTarget = event.path && event.path.length > 0 ? event.path[0] : event.target;
+        if (container.contains(eventTarget)) return;
+        arrelementsmatch = [];
+        tag = '';
+        liIndex = -1;
+    }
+
+    function handleKeyPress(event) {
+        if(!arrelementsmatch || arrelementsmatch.length === 0) return;
+        const keyCode = event.keyCode;
+        let el;
+        switch (keyCode) {
+            case CODES.ARROW_UP:
+                el = document.getElementById(`li-${liIndex}`);
+                if(el) el.classList.remove('active');
+                liIndex--;
+                el = document.getElementById(`li-${liIndex}`);
+                if(el) { 
+                    el.classList.add('active');
+                }
+                break;
+        
+            case CODES.ARROW_DOWN:
+                el = document.getElementById(`li-${liIndex}`);
+                if(el) el.classList.remove('active');
+                liIndex++;
+                el = document.getElementById(`li-${liIndex}`);
+                if(el) {
+                    el.classList.add('active');
+                }
+                break;
+        }
+    }
+
+    function _trimTrailing(word='', char=',') {
+        let last = word[word.length - 1];
+        if(char !== last) return word;
+        return word.substring(0, word.length - 1);
     }
 
 </script>
+<svelte:window on:click={handleWindowClick}
+on:keydown={handleKeyPress} />
 
-<div class="svelte-tags-input-layout">
-    {#if tags.length > 0}
-        {#each tags as tag, i}
-            <span class="svelte-tags-input-tag">{tag} <span class="svelte-tags-input-tag-remove" on:click={() => removeTag(i)}> ×</span></span>
-        {/each}
+<div bind:this={container}>
+    <div class="svelte-tags-input-layout">
+        {#if tags.length > 0}
+            {#each tags as tag, i}
+                <span class="svelte-tags-input-tag tag {tag}">{tag} 
+                    <span class="svelte-tags-input-tag-remove" on:click={_ => removeTag(i)}> ×</span>
+                </span>
+            {/each}
+        {/if}
+        <input  type="text" 
+                bind:value={tag} 
+                on:keydown={setTag} 
+                on:keyup={getMatchElements} 
+                on:paste={onPaste} 
+                on:drop={onDrop} 
+                class="svelte-tags-input" 
+                placeholder={placeholder} />
+        
+    </div>
+
+    {#if autoComplete && arrelementsmatch.length > 0}
+        <ul class="svelte-tags-input-matchs">
+            {#each arrelementsmatch as element, i}
+            <li id="li-{i}" value="{element}" on:click={_ => addTagFromDropdown(element, element)}>{element}</li>
+            {/each}
+        </ul>
     {/if}
-    <input type="text" bind:value={tag} on:keydown={setTag} on:keyup={getMatchElements} on:paste={onPaste} on:drop={onDrop} class="svelte-tags-input" placeholder={placeholder}>
-    
 </div>
-
-{#if autoComplete && arrelementsmatch.length > 0}
-    <ul class="svelte-tags-input-matchs">
-        {#each arrelementsmatch as element, i}
-        <li value="element" on:click={_=>addTag(element)}>{element}</li>
-        {/each}
-    </ul>
-{/if}
 
 <style>
 /* main */
-.svelte-tags-input,
-.svelte-tags-input-tag {
-    font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Fira Sans","Droid Sans","Helvetica Neue",sans-serif;
+.svelte-tags-input {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
     font-size: 14px;
     padding: 2px 5px;
 }
@@ -186,13 +301,14 @@
             align-items:center;
     padding: 0px 5px 5px 5px;
     border: solid 1px #CCC;
-    background: #FFF;
-    border-radius: 2px;
+    background: #f6f6fc;
+    border-radius: 4px;
+    box-shadow: 0 0 0 2px rgba(0,0,0,.1);
 }
 
 .svelte-tags-input-layout:focus,
 .svelte-tags-input-layout:hover {
-    border: solid 1px #000;    
+    border: solid 1px #252529;    
 }
 
 /* svelte-tags-input */
@@ -203,6 +319,7 @@
     margin: 0;
     margin-top: 5px;
     border:none;
+    background: #f6f6fc;
 }
 
 .svelte-tags-input:focus {
@@ -211,14 +328,31 @@
 
 /* svelte-tags-input-tag */
 .svelte-tags-input-tag {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    font-size: 12px;
+    font-weight: 600;
+    padding: 4px 5px 5px 5px;
+}
+
+.svelte-tags-input-tag {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Opera and Firefox */
+}
+
+.svelte-tags-input-tag {
     display:-webkit-box;
     display:-ms-flexbox;
     display:flex;
     white-space: nowrap;
     list-style:none;
-    background: #000;
-    color: #FFF;
-    border-radius: 2px;
+    background: #252529;
+    color: #f6f6fc;
+    border-radius: 4px;
     margin-right: 5px;
     margin-top: 5px;
 }
@@ -229,28 +363,35 @@
 
 .svelte-tags-input-tag-remove {
     cursor:pointer;
+    padding-left: 4px;
 }
 
 /* svelte-tags-input-matchs */
 .svelte-tags-input-matchs {
-    margin:3px 0;
+    margin: 3px 0;
     padding: 0px;
     border: solid 1px #CCC;
-    border-radius: 2px;
-    max-height:310px;
-    overflow:scroll;
-    overflow-x:hidden;
+    border-radius: 0 0 4px 4px;
+    max-height: 310px;
+    overflow: scroll;
+    overflow-x: hidden;
 }
 
 .svelte-tags-input-matchs li {
-    list-style:none;
-    padding:5px;
-    border-radius: 2px;
-    cursor:pointer;
+    list-style: none;
+    padding: 5px;
+    /* border-radius: 2px; */
+    cursor: pointer;
+    background:#f6f6fc;
+}
+
+.svelte-tags-input-matchs  li.active  {
+    color:#f6f6fc;
+    background:#252529 !important;
 }
 
 .svelte-tags-input-matchs li:hover {
-    background:#000;
-    color:#FFF;
+    background:#252529;
+    color:#f6f6fc;
 }
 </style>
