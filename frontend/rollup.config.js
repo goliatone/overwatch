ec9496a0090fff5b3654c5c070d5d90c13855cd6
goliatone <burgosemi@gmail.com>
2020-04-20 22:58:14 -0700

    update rollup config to move public items to build dir

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -3,25 +3,27 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
     input: 'src/main.js',
     output: {
-        sourcemap: true,
+        sourcemap: !production,
         format: 'iife',
         name: 'app',
-        file: 'public/build/bundle.js'
+        file: 'build/bundle.js'
     },
     plugins: [
+        copy({ targets: [{ src: 'public/*', dest: 'build' }] }),
         svelte({
             // enable run-time checks when not in production
             dev: !production,
             // we'll extract any component CSS out into
             // a separate file - better for performance
             css: css => {
-                css.write('public/build/bundle.css');
+                css.write('build/bundle.css', !production);
             }
         }),
 
@@ -38,11 +40,14 @@ export default {
 
         // In dev mode, call `npm run start` once
         // the bundle has been generated
-        !production && serve(),
+        !production && serve({
+            contentBase: ['build'],
+            port: 5000
+        }),
 
         // Watch the `public` directory and refresh the
         // browser on changes when not in production
-        !production && livereload('public'),
+        !production && livereload({ watch: 'build' }),
 
         // If we're building for production (npm run build
         // instead of npm run dev), minify
